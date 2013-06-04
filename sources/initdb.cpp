49d2005510bdd496d24628e434fb7e90e685d5a5
vincent xuereb <vinc.xuereb@laposte.net>
2013-06-04 14:12:36 +0200

    debut de la db

---
+++ sources/initdb.cpp
@@ -0,0 +1,37 @@
+#include <future>
+#include <bitcoin/bitcoin.hpp>
+using namespace bc;
+
+int main(int argc, char** argv)
+{
+    if (argc != 2 )
+    {
+	std::cout << "erreur argc != 2" << std::endl;
+	return 1;
+    }
+    const std::string dbpath = argv[1];
+    threadpool pool(1);
+    leveldb_blockchain chain(pool);
+    auto blockchain_start = [](const std::error_code& ec) {}; 
+    
+    chain.start(dbpath, blockchain_start);
+    block_type first_block = genesis_block();
+    std::promise<std::error_code> ec_promise;
+    auto import_finished =
+        [&ec_promise](const std::error_code& ec)
+        {
+            ec_promise.set_value(ec);
+        };
+   chain.import(first_block, 0, import_finished); 
+   std::error_code ec = ec_promise.get_future().get();
+   if (ec)
+   {
+        log_error() << "Importing genesis block failed: " << ec.message();
+        return -1;
+   }
+  log_info() << "Imported genesis block " << hash_block_header(first_block);
+  pool.stop();
+  pool.join();
+  chain.stop();
+  return 0;
+}
